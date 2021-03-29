74db71cbddb3ca0d7cc2600ded8962ba0b563bdf
GhOsT <armanjawed2000@gmail.com>
2021-03-29 09:50:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing some libraries
 import discord
 from discord.ext import commands
 from discord import Intents
@@ -8,27 +9,31 @@ import json
 from utils.util import get_prefix
 from datetime import datetime, timedelta
 
+#loads the secret file containg the bot's password
 load_dotenv()
 
 intents = Intents.default()
 intents.members = True
 
+# defining a few things
 bot = commands.Bot(command_prefix=get_prefix,
                    owner_id=722168161713127435,
                    intents=discord.Intents.all(),
                    help_command=None)
 
+# extenstion list
 cogs = ["cogs.mod", "cogs.fun", "cogs.utility", "cogs.help", "cogs.errors"]
 
+# loads the extensions apon starting the bot
 for cog in cogs:
     bot.load_extension(cog)
 
-
+# Set's the bot's status
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game('..help'))
 
-
+# When the bot joins a server, it adds the server to the database, along with it's prefix
 @bot.event
 async def on_guild_join(guild):
 
@@ -40,7 +45,7 @@ async def on_guild_join(guild):
     with open("prefixes.json", "w") as f:
         json.dump(prefixes, f)
 
-
+# Command group for performing bot admin actions
 @bot.group()
 @commands.is_owner()
 async def sudo(ctx):
@@ -48,14 +53,14 @@ async def sudo(ctx):
         await ctx.send(
             "Use `..sudo <command>` to perform your bot admin actions.")
 
-
+# load an extension
 @sudo.command()
 @commands.is_owner()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f"Sucessfully loaded the `{extension}` Cog.")
 
-
+# Unload an extension
 @sudo.command()
 @commands.is_owner()
 async def unload(ctx, extension):
@@ -64,7 +69,7 @@ async def unload(ctx, extension):
         f"Successfully unloaded the `{extension}` Cog.\nLol why are u making me more useless?"
     )
 
-
+# Reload an extension
 @sudo.command()
 @commands.is_owner()
 async def reload(ctx, extension):
@@ -72,15 +77,17 @@ async def reload(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f"Done. Reloaded The `{extension}` Cog.")
 
-
+# Shuts the bot down by logging out
 @sudo.command()
 @commands.is_owner()
 async def shutdown(ctx):
     await ctx.send("logging out... bye!")
     await ctx.bot.logout()
 
+# local variable for the command below
 bot.launch_time = datetime.utcnow()
 
+# Gives how long the bot has been running
 @bot.command()
 @commands.guild_only()
 @commands.cooldown(1, 10, commands.BucketType.user)
@@ -91,5 +98,8 @@ async def uptime(ctx):
   days, hours = divmod(hours, 24)
   await ctx.send(f"The bot has been up for`{days}`days , `{hours}`hours and `{minutes}`minutes.")
 
+# Runs the flask file
 keep_alive()
+
+# Logs the bot into discord
 bot.run(getenv("TOKEN"))
