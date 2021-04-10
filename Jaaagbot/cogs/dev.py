8d4f9c3da84211506452d96490d536877b66bc4e
GhOsT <>
2021-04-10 15:28:07 +0000

    Might aswell do this

--- Jaaagbot/cogs/dev.py
+++ Jaaagbot/cogs/dev.py
@@ -13,75 +13,155 @@ class Dev(commands.Cog):
     # Prints on the console when the extension is loaded
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+        print(
+          "{} Cog has been loaded\n-----".format(
+            self.__class__.__name__
+            )
+        )
 
     # Sends a message to a private channel informing me that it has joined a guild
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         logchannel = await self.bot.fetch_channel(828972017491116032)
-        embed = discord.Embed(color=0x33fcff, timestamp=datetime.utcnow())
-        embed.set_author(name="I have joined a server",
-                         icon_url=f"{self.bot.user.avatar_url}")
-        embed.set_thumbnail(url=f"{guild.icon_url}")
-        embed.add_field(name="**Name:**", value=f"{guild}", inline=False)
-        embed.add_field(name="**Guild ID:**",
-                        value=f"{guild.id}",
-                        inline=False)
-        embed.add_field(name="**Guild Owner:**",
-                        value=f"{guild.owner} ({guild.owner_id})")
-        embed.add_field(name="**Guild created at:**",
-                        value=f"{guild.created_at}",
-                        inline=False)
-        embed.add_field(name="**Member Count:**",
-                        value=f"{guild.member_count}",
-                        inline=False)
-        embed.add_field(name="**Tolal server count now:**",
-                        value=f"{len(self.bot.guilds)} guilds",
-                        inline=False)
+        embed = discord.Embed(
+          color=0x33fcff, 
+          timestamp=datetime.utcnow()
+        )
+        embed.set_author(
+          name="I have joined a server",
+          icon_url="{}".format(
+            self.bot.user.avatar_url
+            )
+        )
+        embed.set_thumbnail(
+          url="{}".format(
+            guild.icon_url
+            )
+        )
+        embed.add_field(
+          name="**Name:**", 
+          value="{}".format(
+            guild
+            ), 
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild ID:**",
+          value="{}".format(
+            guild.id
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild Owner:**", 
+          value="{} ({})".format(
+            guild.owner, guild.owner_id
+            ), 
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild created at:**",
+          value="{}".format(
+            guild.created_at
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Member Count:**",
+          value="{}".format(
+            guild.member_count
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Tolal server count now:**",
+          value="{} guilds".format(
+            len(self.bot.guilds)
+            ),
+          inline=False
+        )
         await logchannel.send(embed=embed)
 
     # Sends a message to a private channel informing me that it has left a guild.
     @commands.Cog.listener()
     async def on_guild_leave(self, guild):
         logchannel = await self.bot.fetch_channel(828972017491116032)
-        embed = discord.Embed(color=0x33fcff, timestamp=datetime.utcnow())
-        embed.set_author(name="I have left a server",
-                         icon_url=f"{self.bot.user.avatar_url}")
-        embed.set_thumbnail(url=f"{guild.icon_url}")
-        embed.add_field(name="**Name:**", value=f"{guild}", inline=False)
-        embed.add_field(name="**Guild ID:**",
-                        value=f"{guild.id}",
-                        inline=False)
-        embed.add_field(name="**Guild Owner:**",
-                        value=f"{guild.owner} ({guild.owner_id})")
-        embed.add_field(name="**Guild created at:**",
-                        value=f"{guild.created_at}",
-                        inline=False)
-        embed.add_field(name="**Member Count:**",
-                        value=f"{guild.member_count}",
-                        inline=False)
-        embed.add_field(name="**Tolal server count now:**",
-                        value=f"{len(self.bot.guilds)} guilds",
-                        inline=False)
+        embed = discord.Embed(
+          color=0x33fcff, 
+          timestamp=datetime.utcnow()
+        )
+        embed.set_author(
+          name="I have left a server",
+          icon_url="{}".format(
+            self.bot.user.avatar_url
+            )
+        )
+        embed.set_thumbnail(
+          url="{}".format(
+            guild.icon_url
+            )
+        )
+        embed.add_field(
+          name="**Name:**", 
+          value="{}".format(
+            guild
+            ), 
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild ID:**",
+          value="{}".format(
+            guild.id
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild Owner:**",
+          value="{} ({})".format(
+            guild.owner, guild.owner_id
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Guild created at:**",
+          value="{}".format(
+            guild.created_at
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Member Count:**",
+          value="{}".format(
+            guild.member_count
+            ),
+          inline=False
+        )
+        embed.add_field(
+          name="**Tolal server count now:**",
+          value="{} guilds".format(
+            len(self.bot.guilds)
+            ),
+          inline=False
+        )
         await logchannel.send(embed=embed)
 
     # A command that makes the bot leave a server. Only works for the bot owner(me).
-    @commands.command(aliases=["leave"])
+    @commands.command(aliases=["remove"])
     @commands.is_owner()
-    async def leaveguild(self, ctx, id: str):
-        if id == 'this':
-            await ctx.send("Ok. I am leaving this guild.")
-            await ctx.guild.leave()
-            return
-        else:
-            guild = self.bot.get_guild(id)
-            if guild:
-                await guild.leave()
-                msg = f"Ok. I am leaving {guild.name}"
-            else:
-                msg = "I couldn't find a guild matching this ID!"
-        await ctx.send(msg)
-
+    async def leave(self, ctx, id: str):
+      if id == 'this':
+        await ctx.send(
+          "Ok. I am leaving this guild."
+          )
+        await ctx.guild.leave()
+      else:
+        guild = self.bot.get_guild(id)
+        await guild.leave()
+        await ctx.send(
+          "Done. I have left {} ({})".format(
+            guild.name, guild.id
+            )
+        )
 
 # Adds the extention
 def setup(bot: commands.Bot):
