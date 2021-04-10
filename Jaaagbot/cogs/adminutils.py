178b85c93835c4980655e392fea31cd7d453e925
GhOsT <>
2021-04-10 14:30:44 +0000

    Make it more beautiful

--- Jaaagbot/cogs/adminutils.py
+++ Jaaagbot/cogs/adminutils.py
@@ -18,23 +18,34 @@ class Adminutils(commands.Cog):
     # Prints on the console when the extension is loaded
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+        print(
+          "{} Cog has been loaded\n-----".format(
+            self.__class__.__name__
+            )
+        )
 
     # A command to set the guild prefix for the bot
     @commands.command(aliases=["pre"])
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
     async def setprefix(self, ctx, prefix):
-        await self.bot.config.upsert({"_id": ctx.guild.id, "prefix": prefix})
+        await self.bot.config.upsert(
+          {"_id": ctx.guild.id, "prefix": prefix}
+        )
         await ctx.send(
-            f"The guild prefix has been set to `{prefix}`. Use `{prefix}setprefix [prefix]` to change it again!")
+          "The guild prefix has been set to `{}`. Use `{}setprefix [prefix]` to change it again!".format(
+            prefix, prefix
+            )
+        )
     
     # A command to reset the guild prefix for the bot
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
     async def resetprefix(self, ctx):
-        await self.bot.config.unset({"_id": ctx.guild.id, "prefix": 1})
+        await self.bot.config.unset(
+          {"_id": ctx.guild.id, "prefix": 1}
+        )
         await ctx.send(
           "The prefix for this guild has been reset to `..`"
         )
@@ -53,7 +64,9 @@ class Adminutils(commands.Cog):
     async def snipe(self, ctx):
         """A command to snipe delete messages."""
         if not self.last_msg:
-            await ctx.send("There is no message to snipe!")
+            await ctx.send(
+              "There is no message to snipe!"
+            )
             return  
 
         author = self.last_msg.author
@@ -75,8 +88,14 @@ class Adminutils(commands.Cog):
     async def modlogset(self, ctx, channel: discord.TextChannel=None):
       if channel is None:
         channel=ctx.channel
-      await self.bot.modlog.upsert({"_id": ctx.guild.id, "modlog": channel.id})
-      await ctx.send(f"Modlog events will now be posted to {channel.mention}.")
+      await self.bot.modlog.upsert(
+        {"_id": ctx.guild.id, "modlog": channel.id}
+      )
+      await ctx.send(
+        "Modlog events will now be posted to {}.".format(
+          channel.mention
+          )
+      )
 
     # A command to set the muterole for the guild. this will be used in the updated mute commmand
     @commands.command(aliases=["mrs", "muteset"])
@@ -86,10 +105,18 @@ class Adminutils(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def muteroleset(self, ctx, role: discord.Role=None):
       if role is None:
-        await ctx.send("You need do mention a role to be set as the muterole.")
+        await ctx.send(
+          "You need do mention a role to be set as the muterole."
+        )
       else:
-        await self.bot.muterole.upsert({"_id": ctx.guild.id, "muterole": role.id})
-        await ctx.send(f"The muterole has been set to {role.mention}")
+        await self.bot.muterole.upsert(
+          {"_id": ctx.guild.id, "muterole": role.id}
+        )
+        await ctx.send(
+          "The muterole has been set to {}".format(
+            role.mention
+            )
+        )
 
 
 # Adds the extention
